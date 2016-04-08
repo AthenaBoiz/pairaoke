597d94cad24e4445ec859ab321a893cdec6aa8d5
Terry Capan <tcapanjr@gmail.com>
2016-04-08 16:28:33 -0500

    add front end state listener and other auth functionality

--- public/src/app/components/auth/auth.js
+++ public/src/app/components/auth/auth.js
@@ -5,39 +5,31 @@ angular.module('karaoke.auth', [])
   $scope.user = {};
 
   $scope.signup = function(isValid) {
-    authFactory.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.karaoke', token);
-        //direct to where upon signup? 
-        $location.path('/event');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-    // if (isValid) {
-    //   // send user info to server
-    //   // if successful, tell user, start a session. set any info on $rootScope?
-    //   // else, indicate the error (user name already taken, etc.)
-    //   console.log('can submit');
-    // }
+    if (isValid) {
+      authFactory.signup($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.karaoke', token);
+          //direct to where upon signup? 
+          $location.path('/addEvent');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   };
 
   $scope.login = function(isValid) {
-    Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.karaoke', token);
-        //direct to where upon login? 
-        $location.path('/event');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-    // if (isValid) {
-    //   // send user info to server
-    //   // if successful, start a session
-    //   // else, indicate the error (un does not exist, wrong password, etc.)
-    //   console.log('can submit');
-    // }
+    if (isValid) {
+      Auth.signin($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.karaoke', token);
+          //direct to where upon login? 
+          $location.path('/addEvent');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   };
 
 });
