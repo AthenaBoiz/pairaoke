1c0b5a2f73b32a2c9e004c24aa0299ed0605dcea
Terry Capan <tcapanjr@gmail.com>
2016-04-12 00:44:30 -0500

    add user service for password auth and token return

--- public/src/app/components/auth/auth.js
+++ public/src/app/components/auth/auth.js
@@ -9,7 +9,7 @@ angular.module('karaoke.auth', [])
       authFactory.signup($scope.user)
         .then(function (token) {
           $window.localStorage.setItem('com.karaoke', token);
-          //direct to where upon signup? 
+          //direct to where upon signup?
           $location.path('/addEvent');
         })
         .catch(function (error) {
@@ -20,14 +20,10 @@ angular.module('karaoke.auth', [])
 
   $scope.login = function(isValid) {
     if (isValid) {
-<<<<<<< HEAD
-      Auth.signin($scope.user)
-=======
       authFactory.login($scope.user)
->>>>>>> ef37efd16c486175a9dc9abef5978af1d1d0965b
         .then(function (token) {
           $window.localStorage.setItem('com.karaoke', token);
-          //direct to where upon login? 
+          //direct to where upon login?
           $location.path('/addEvent');
         })
         .catch(function (error) {
